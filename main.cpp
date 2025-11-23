7e946e3117d8e63f64ce7763162ce59945444a50
Henryawd345 <henrygoodboy2000@gmail.com>
2025-11-23 02:43:59 -0800

    update

--- main.cpp
+++ main.cpp
@@ -11,9 +11,30 @@ private:
     vector<vector<pair<int, int>>> adj;
 
 public:
-    
+    Graph(int vertices) : V(vertices){
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v, int w){
+        adj[u].push_back({v, w});
+        adj[v].push_back({u, w});
+
+    }
+
+    void printAdjacencyList() const{
+        cout << "Graph's adjacency list: " << endl;
+        for (int i = 0; i < V; i++){
+            cout << i << " --> ";
+            for (const auto &edge : adj[i]){
+                cout << "(" << edge.first << ", " << edge.second << ") ";
+            }
+            cout << endl;
+            
+        }
+        
 
 
+    }
 
 }
 
