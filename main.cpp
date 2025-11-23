f9ad39fb3d5d1b79eef3ce9b36ea256c007ba69d
Henryawd345 <henrygoodboy2000@gmail.com>
2025-11-23 03:00:13 -0800

    update

--- main.cpp
+++ main.cpp
@@ -88,7 +88,25 @@ public:
 
 int main() {
 
+    Graph g(7);
 
+    
+    g.addEdge(0, 1, 12);
+    g.addEdge(0, 2, 8);
+    g.addEdge(0, 3, 21);
 
+    g.addEdge(2, 3, 6);
+    g.addEdge(2, 6, 2);
+    g.addEdge(2, 4, 4);
+    g.addEdge(2, 5, 5);
+    
+    g.addEdge(4, 5, 9);
 
+    g.addEdge(5, 6, 6);
+
+    g.printAdjacencyList();
+    g.DFS(0);
+    g.BFS(0);
+
+    return 0;
 }
